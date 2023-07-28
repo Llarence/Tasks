b995a8c8a488ad21d390c29c01db4e37f2a907a3
Llarence <you@example.com>
2023-07-28 10:29:17 -0700

    simple calendar

---
+++ common/src/commonMain/kotlin/me/llarence/common/Constants.kt
@@ -0,0 +1,16 @@
+package me.llarence.common
+
+import androidx.compose.ui.unit.dp
+import java.net.URL
+
+const val HOURS = 24
+const val DAYS = 7
+
+const val HOUR_SIZE = 100f
+const val DAY_PADDING = 10f
+
+const val TEXT_PADDING = 2f
+
+const val CORNER_RADIUS = 10f
+
+const val GRAB_EXPAND = 4f
--- common/src/commonMain/kotlin/me/llarence/common/Platform.kt
+++ common/src/commonMain/kotlin/me/llarence/common/Platform.kt
@@ -1,5 +1,6 @@
 package me.llarence.common
 
+import androidx.compose.ui.unit.dp
 import java.net.URL
 
 expect val platformName: String
